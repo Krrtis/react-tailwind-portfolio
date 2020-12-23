aac577a00eec10657c9b0b674c0d2a9dba578473
kurtis <kurtisgibb@icloud.com>
2020-12-22 22:41:03 -0500

    added toggle darkmode button

--- src/App.js
+++ src/App.js
@@ -1,74 +1,86 @@
+import React from 'react';
 import NavBar from './NavBar';
 import Hero from './Hero';
 import Footer from './Footer';
 import './App.css';
 
-function App() {
-	return (
-		<div className="App">
-			<header>
-				<nav>
-					<NavBar />
-				</nav>
-			</header>
-			<main>
-				<section id="hero">
-					<Hero />
-				</section>
-				<section id="main">
-					<div className="container mx-auto px-6 p-10">
-						<h2 className="text-4xl font-bold text-center text-gray-800 mb-8">
-							Header
-						</h2>
-						<div className="flex items-center flex-wrap mb-20">
-							<div className="w-full md:w-1/2">
-								<h4 className="text-3xl text-gray-800 font-bold mb-3">
-									A subheader
-								</h4>
-								<p className="text-gray-600 mb-8">
-									information for the subheader
-								</p>
-							</div>
-							<div className="w-full md:w-1/2">
-								<p>insert an image here</p>
-							</div>
-						</div>
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = { darkMode: false };
+	}
 
-						<div className="flex items-center flex-wrap mb-20">
-							<div className="w-full md:w-1/2">
-								<p>insert an image here</p>
-							</div>
-							<div className="w-full md:w-1/2">
-								<h4 className="text-3xl text-gray-800 font-bold mb-3">
-									Second subheader
-								</h4>
-								<p className="text-gray-600 mb-8">
-									yay for a second subheader paragraph
-								</p>
-							</div>
-						</div>
+	toggleDarkMode = (darkMode) => {
+		this.setState({ darkMode: !this.state.darkMode });
+	};
 
-						<div className="flex items-center flex-wrap mb-20">
-							<div className="w-full md:w-1/2">
-								<h4 className="text-3xl text-gray-800 font-bold mb-3">
-									Third and final subheader
-								</h4>
-								<p className="text-gray-600 mb-8">
-									this is the last section paragraph, woo
-								</p>
+	render() {
+		return (
+			<div className={`App ${this.state.darkMode ? 'dark' : ''}`}>
+				<header>
+					<nav>
+						<NavBar toggleDarkMode={this.toggleDarkMode} />
+					</nav>
+				</header>
+				<main>
+					<section id="hero">
+						<Hero />
+					</section>
+					<section id="main">
+						<div className="container mx-auto px-6 p-10">
+							<h2 className="text-4xl font-bold text-center text-gray-800 mb-8">
+								Header
+							</h2>
+							<div className="flex items-center flex-wrap mb-20">
+								<div className="w-full md:w-1/2">
+									<h4 className="text-3xl text-gray-800 font-bold mb-3">
+										A subheader
+									</h4>
+									<p className="text-gray-600 mb-8">
+										information for the subheader
+									</p>
+								</div>
+								<div className="w-full md:w-1/2">
+									<p>insert an image here</p>
+								</div>
 							</div>
-							<div className="w-full md:w-1/2">
-								<p>another image here</p>
+
+							<div className="flex items-center flex-wrap mb-20">
+								<div className="w-full md:w-1/2">
+									<p>insert an image here</p>
+								</div>
+								<div className="w-full md:w-1/2">
+									<h4 className="text-3xl text-gray-800 font-bold mb-3">
+										Second subheader
+									</h4>
+									<p className="text-gray-600 mb-8">
+										yay for a second subheader paragraph
+									</p>
+								</div>
+							</div>
+
+							<div className="flex items-center flex-wrap mb-20">
+								<div className="w-full md:w-1/2">
+									<h4 className="text-3xl text-gray-800 font-bold mb-3">
+										Third and final subheader
+									</h4>
+									<p className="text-gray-600 mb-8">
+										this is the last section paragraph, woo
+									</p>
+								</div>
+								<div className="w-full md:w-1/2">
+									<p>another image here</p>
+								</div>
 							</div>
 						</div>
-					</div>
-				</section>
-			</main>
-			<footer>
-				<Footer />
-			</footer>
-		</div>
-	);
+					</section>
+				</main>
+				<footer>
+					<Footer />
+				</footer>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,24 +1,45 @@
 import React from 'react';
 
-const NavBar = () => {
-	return (
-		<div className="container mx-auto px-10 py-2 flex justify-end">
-			<div className="block">
-				<ul className="inline-flex">
-					<li>
-						<a className="px-4 hover:text-gray-600" href="/">
-							Object
-						</a>
-					</li>
-					<li>
-						<a className="px-4 hover:text-gray-600" href="/">
-							Object
-						</a>
-					</li>
-				</ul>
+class NavBar extends React.Component {
+	render() {
+		return (
+			<div className="container mx-auto px-10 py-2 flex justify-between bg-white dark:bg-black bg-opacity-60">
+				<div className="block">
+					<button
+						aria-label="Toggle dark mode"
+						type="button"
+						className="bg-gray-200 dark:bg-gray-800 rounded p-3 h-10 w-10"
+						onClick={this.props.toggleDarkMode}>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							className="h-4 w-4 text-gray-800 dark:text-gray-200">
+							<path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+						</svg>
+					</button>
+				</div>
+				<div className="block">
+					<ul className="inline-flex">
+						<li>
+							<a
+								className="px-4 text-gray-900 dark:text-gray-100"
+								href="/">
+								Object
+							</a>
+						</li>
+						<li>
+							<a
+								className="px-4 text-gray-900 dark:text-gray-100"
+								href="/">
+								Object
+							</a>
+						</li>
+					</ul>
+				</div>
 			</div>
-		</div>
-	);
-};
+		);
+	}
+}
 
 export default NavBar;
